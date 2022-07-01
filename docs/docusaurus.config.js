90342836f92ceacbfe13e3020b8555db15b66edd
Robin Ohs <s8roohss@stud.uni-saarland.de>
2022-07-01 18:27:08 +0200

    Add trailingSlash

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -18,7 +18,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'robinohs', // Usually your GitHub org/user name.
   projectName: 'totp-kt', // Usually your repo name.
-
+  trailingSlash: false,
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
