80e1345e61d9cbb71eed722cca70b3b48d81f156
Robin Ohs <s8roohss@stud.uni-saarland.de>
2023-02-18 23:18:59 +0100

    Add publish tasks for maven central.

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -1,13 +1,17 @@
 description = ""
 group = "dev.robinohs"
-version = "1.0.3"
+version = "1.0.1-SNAPSHOT"
+
+val sonatypeUsername: String? = System.getenv("SONATYPE_USERNAME")
+val sonatypePassword: String? = System.getenv("SONATYPE_PASSWORD")
 
 plugins {
-    id("org.jetbrains.kotlin.jvm") version "1.7.0"
+    id("org.jetbrains.kotlin.jvm") version "1.8.10"
     id("jacoco")
     id("org.sonarqube") version "3.3"
     id("java-library")
     id("maven-publish")
+    id("signing")
 }
 
 repositories {
@@ -32,6 +36,7 @@ tasks.jar {
 }
 
 java {
+    withJavadocJar()
     withSourcesJar()
 }
 
@@ -58,12 +63,63 @@ tasks {
 
 publishing {
     publications {
-        create<MavenPublication>("maven") {
-            groupId = project.group as String
-            artifactId = "totpkt"
-            version = project.version as String
-
+        create<MavenPublication>("mavenJava") {
+            artifactId = "totp-kt"
             from(components["java"])
+            versionMapping {
+                usage("java-api") {
+                    fromResolutionOf("runtimeClasspath")
+                }
+                usage("java-runtime") {
+                    fromResolutionResult()
+                }
+            }
+            pom {
+                name.set("totp-kt")
+                description.set("Native Kotlin library for time-based TOTP and HMAC-based HOTP one-time passwords.")
+                url.set("https://github.com/robinohs/totp-kt")
+                licenses {
+                    license {
+                        name.set("The MIT License (MIT)")
+                        url.set("https://mit-license.org/")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("Roboh97")
+                        name.set("Robin Ohs")
+                        email.set("info@robinohs.dev")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:https://github.com/robinohs/totp-kt.git")
+                    developerConnection.set("scm:git:https://github.com/robinohs/totp-kt.git")
+                    url.set("https://github.com/robinohs/totp-kt")
+                }
+            }
         }
+    }
+    repositories {
+        maven {
+            // change URLs to point to your repos, e.g. http://my.org/repo
+            name="oss"
+            val releasesRepoUrl = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+            val snapshotsRepoUrl = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+            url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
+            credentials {
+                username = sonatypeUsername
+                password = sonatypePassword
+            }
+        }
+    }
+}
+
+signing {
+    sign(publishing.publications["mavenJava"])
+}
+
+tasks.javadoc {
+    if (JavaVersion.current().isJava9Compatible) {
+        (options as StandardJavadocDocletOptions).addBooleanOption("html5", true)
     }
 }
